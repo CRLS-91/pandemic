925d4d6479eed7c75ec8f26a3b058606c8b13839
melll1 <147088494+melll1@users.noreply.github.com>
2024-10-27 16:32:10 +0100

    commit
    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './autentificacion/inicio/inicio.component';
 import { PerfilusuarioComponent } from './perfil/perfilusuario/perfilusuario.component';
 import { LoginComponent } from './autentificacion/login/login.component';
+import { RegistroComponent } from './autentificacion/registro/registro.component'
 InicioComponent
 export const routes: Routes = [
 {path:'',component:InicioComponent},
-{path:'login', component: LoginComponent}
+{path:'login', component: LoginComponent},
+{path:'registro', component: RegistroComponent},
 
 ];
--- src/app/autentificacion/registro/registro.component.ts
+++ src/app/autentificacion/registro/registro.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';  // Import CommonModule
+
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+
+  irALogin() {
+    throw new Error('Method not implemented.');
+    }
+    username: string = '';
+    email: string = '';
+    password: string = '';
+    confirmPassword: string = '';
+
+      constructor(private router: Router) {} // Inyección de Router
+    
+      login() {
+        
+        console.log('Nombre de usuario:', this.username);
+        console.log('Email:', this.email);
+        console.log('Password:', this.password);
+        console.log('Confirmar Contraseña:', this.confirmPassword);
+        
+      }
+     
+      vamosLogin() {
+        this.router.navigate(['/login']); 
+      }
 
 }
