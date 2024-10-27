d2b7adb4ae404bcdce978961888c3475d855a1c0
CRLS-91 <147090900+CRLS-91@users.noreply.github.com>
2024-10-27 12:50:24 +0100

    .
    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { PerfilusuarioComponent } from "./perfil/perfilusuario/perfilusuario.component";
+import { InicioComponent } from "./autentificacion/inicio/inicio.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PerfilusuarioComponent],
+  imports: [RouterOutlet, RouterLink, PerfilusuarioComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './autentificacion/inicio/inicio.component';
+import { PerfilusuarioComponent } from './perfil/perfilusuario/perfilusuario.component';
+import { LoginComponent } from './autentificacion/login/login.component';
+InicioComponent
+export const routes: Routes = [
+{path:'',component:InicioComponent},
+{path:'login', component: LoginComponent}
 
-export const routes: Routes = [];
+];
