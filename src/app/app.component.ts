a405bc8140daddc00a46b46b19ed0f60373a58eb
CRLS-91 <147090900+CRLS-91@users.noreply.github.com>
2024-11-05 21:33:43 +0100

    .
    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { InicioComponent } from "./autentificacion/inicio/inicio.component";
 import { SeleccionComponent } from "./menu/seleccion/seleccion.component";
 import { InformacionComponent } from "./menu/informacion/informacion.component";
 import { AutoresComponent } from "./menu/autores/autores.component";
+import { VersionComponent } from "./menu/version/version.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, InicioComponent, SeleccionComponent, InformacionComponent, AutoresComponent],
+  imports: [RouterOutlet, RouterLink, InicioComponent, SeleccionComponent, InformacionComponent, AutoresComponent, VersionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
