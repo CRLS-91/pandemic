a87e6f5fafe53a7a8772e456716231ab5a03cbda
melll1 <147088494+melll1@users.noreply.github.com>
2024-11-06 20:58:23 +0100

    melisa
    melisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { SeleccionComponent } from "./menu/seleccion/seleccion.component";
 import { InformacionComponent } from "./menu/informacion/informacion.component";
 import { AutoresComponent } from "./menu/autores/autores.component";
 import { VersionComponent } from "./menu/version/version.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, InicioComponent, SeleccionComponent, InformacionComponent, AutoresComponent, VersionComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './autentificacion/inicio/inicio.component';
 
 import { LoginComponent } from './autentificacion/login/login.component';
-import { RegistroComponent } from './autentificacion/registro/registro.component'
+
+
+
+
+
 InicioComponent
 export const routes: Routes = [
 {path:'',component:InicioComponent},
 {path:'login', component: LoginComponent},
 
-{path:'registro', component: RegistroComponent},
+{path:'auth',loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
 
 ];
